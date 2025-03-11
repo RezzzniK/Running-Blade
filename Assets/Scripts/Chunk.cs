bc36b7ff35bfda11e3d711cea3cc15603b4bde9b
RezzzniK <reznikblack@gmail.com>
2025-03-11 21:25:10 +0200

    Fence Obstacle with different pos

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] GameObject fencePrefab;//reference for fence prefab
+    [SerializeField] float [] lanes={-2.5f,0,2.5f};//start pos on X axis of the lanes
+
     void Start()
     {
-        
+        SpawnFence();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    //func for spawn fences
+    void SpawnFence(){
+        Instantiate(
+            fencePrefab,
+            new Vector3(lanes[Random.Range(0,lanes.Length)],transform.position.y,transform.position.z),//transform.positin is a position of the level chunk
+            Quaternion.identity,
+            this.transform
+        );
     }
+  
 }
