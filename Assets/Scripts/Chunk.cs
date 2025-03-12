1101a4e0e26f57fe8f483eb28cb88a35106b52c7
RezzzniK <reznikblack@gmail.com>
2025-03-12 23:26:07 +0200

    Pick up power ups

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -5,38 +5,95 @@ public class Chunk : MonoBehaviour
 {
 
     [SerializeField] GameObject fencePrefab;//reference for fence prefab
+    [SerializeField] GameObject applePrefab;
+    [SerializeField] GameObject coinPrefab;
     [SerializeField] float [] lanes={-2.5f,0,2.5f};//start pos on X axis of the lanes
+    [SerializeField] int coinsInOneLane=5;
+     float spawnChanceApple=.3f;
+     float spawnChanceCoin=.5f;
+    
+     List <int> availibleLanes=new List<int>{0,1,2};//creating list and populate it with values 0,1,2
+    
 
     void Start()
     {
         SpawnFence();
+        SpawnApple();
+        SpawnCoin();
     }
+
+   void SpawnCoin()
+    {
+        if (availibleLanes.Count<=0||Random.value>spawnChanceCoin) return;
+        int coinsQuant=Random.Range(1,coinsInOneLane+1);
+        int laneNum=GetLaneToUse();
+       // while(coinsQuant>=0){
+        for (int i = 0; i < coinsQuant; i++)
+        {
+             Instantiate(
+                    coinPrefab,
+                    new Vector3(
+                        lanes[laneNum],//getting lane index from the list and set to start_x list
+                        transform.position.y,
+                        transform.position.z-5+i*2
+                        ),//transform.position is a position of the level chunk
+                    Quaternion.identity,
+                    this.transform
+                );
+        }
+            
+          //  coinsQuant--;
+        //}
+       
+    }
+
     //func for spawn fences
     void SpawnFence(){
 
-        int fencesQuant=Random.Range(1,lanes.Length+1);//generating wich number of the fences
-        List <int> availibleLanes=new List<int>{0,1,2};//creating list and populate it with values 0,1,2
-        //Debug.Log(fencesQuant);
-        while (fencesQuant>0){
-            int laneToUse=Random.Range(0,availibleLanes.Count);//getting random value to populate lane with
-                                                       //fence
+        int fencesQuant=Random.Range(1,lanes.Length);//generating wich number of the fences
+       
+        while (fencesQuant>0)
+        {
             Instantiate(
                     fencePrefab,
                     new Vector3(
-                        lanes[availibleLanes[laneToUse]],//getting lane index from the list and set to start_x list
+                        lanes[GetLaneToUse()/*getting lane to use*/],//getting lane index from the list and set to start_x list
                         transform.position.y,
                         transform.position.z
-                        ),//transform.positin is a position of the level chunk
+                        ),//transform.position is a position of the level chunk
                     Quaternion.identity,
                     this.transform
             );
-            availibleLanes.RemoveAt(laneToUse);//removing element that is under specific index
-            fencesQuant--;  
-            if (fencesQuant>0 && availibleLanes.Count==0)break;//safe cond 
+            fencesQuant--;
+            if (fencesQuant > 0 && availibleLanes.Count == 0) break;//safe cond 
         }
+
+
+
+    }
+    
+
+    private int GetLaneToUse()
+    {
+        int index=Random.Range(0, availibleLanes.Count);
+        int lane=availibleLanes[index];
+        availibleLanes.RemoveAt(index);//removing element that is under specific index
+        return lane;
+    }
+
+    void SpawnApple(){
+        if (availibleLanes.Count<=0||Random.value>spawnChanceApple) return;
+        Instantiate(
+                    applePrefab,
+                    new Vector3(
+                        lanes[GetLaneToUse()],//getting lane index from the list and set to start_x list
+                        transform.position.y,
+                        transform.position.z
+                        ),//transform.position is a position of the level chunk
+                    Quaternion.identity,
+                    this.transform
+        );
        
-
-
     }
   
 }
