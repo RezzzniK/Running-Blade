dae7df99e12e329400e80f733a08742c460deed3
Alexander Reznik <alexander.reznik@play.works>
2025-04-13 13:41:57 +0300

    Post processing 2

--- Assets/Scripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -6,7 +6,7 @@ public class PlayerCollisionHandler : MonoBehaviour
     [SerializeField] Animator animator;//reference to animator to trigger anim
     [SerializeField] float collisionCoolDown=1f;
     [SerializeField] int collisinoPoints=-50;
-
+    [SerializeField] ParticleSystem player_ps;
     LevelGenerator levelGenerator;//level generator class
     ScoreMG score;
     bool inProgress=false;
@@ -19,6 +19,7 @@ public class PlayerCollisionHandler : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision)
     {
+       
         if(!inProgress){
            
             inProgress=true;
@@ -29,9 +30,16 @@ public class PlayerCollisionHandler : MonoBehaviour
         }
       
     }
+      void OnTriggerEnter(Collider other)
+    {
+         if (other.gameObject.tag=="Apple"){
+                player_ps.Play();
+            }
+    }
 
     IEnumerator hitTimer(){
         yield return new WaitForSeconds(collisionCoolDown);
         inProgress=false;
+    
     }
 }
